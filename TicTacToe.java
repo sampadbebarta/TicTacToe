6290f819a61a4c89834679143f614487a2902399
Sampad <bebartasampad@gmail.com>
2021-03-18 15:37:34 +0530

    Creation of Tic Tac Toe Board

---
+++ TicTacToe.java
@@ -0,0 +1,15 @@
+public class TicTacToeY {
+	private char[] board = new char[10];
+
+	public void boardCreation() {
+		for (int position = 1; position<board.length; position++) {
+			board[position] = ' ';
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Tic Tac Toe game");
+		TicTacToe ticTacToeGame = new TicTacToe();
+		ticTacToeGame.boardCreation();
+	}
+}
